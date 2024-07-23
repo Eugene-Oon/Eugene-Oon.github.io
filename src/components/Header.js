d26a83fbeed3bd2bfd2bd3fde2fec90e55a30349
“Eugene_Oon” <eugeneoon2001@gmail.com>
2024-07-23 16:13:33 +0800

    react push

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const Header = () => {
+
+    return ( 
+        <div className="header_main">   
+            <h1 className="header_title">My TodoList</h1>
+            <p className="header_desc">Track your tasks in this todo list! :D</p>
+
+        </div>
+    );
+}
+ 
+export default Header;
---
+++ src/context/TodoContext.js
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+const TodoContext = createContext({
+    tasks: [],
+    setTasks: () => {}
+})
+
+function TodoProvider({ children }) {
+    const [tasks, setTasks] = useState([
+        
+      ])
+
+      const value = {tasks, setTasks}
+      return (
+        <TodoContext.Provider value = {value}>
+            {children}
+        </TodoContext.Provider>
+      )
+}
+
+export {TodoContext, TodoProvider}
